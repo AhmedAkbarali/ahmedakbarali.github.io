85871dfdaab03ae04907f50fdb4a24d3e285207d
AhmedAkbarali <ahmed.amin.akbarali@gmail.com>
2020-02-07 15:26:15 -0500

    Fixed mobile interfaces

---
+++ src/Pages/ContactMe.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import NavigationBar from "../components/NavigationBar";
+
+import '../components/style/Footer.css'
+
+import {Nav, Navbar} from "react-bootstrap";
+import linkedin from "../images/linkedin.png";
+import github from "../images/github.png";
+import email from "../images/email.png";
+
+class ContactMe extends React.Component {
+    render() {
+        return (
+            <div className="global align-items-center justify-content-center " style={{minHeight: '100vh', textAlign: 'center'}}>
+                <NavigationBar />
+                <p className="section-heading" style={{marginTop: '0', paddingTop: '2em'}}>
+                    Contact
+                </p>
+                <p>
+                    If you would like to contact me feel free to do so using one of the links below.
+                </p>
+                <Navbar style={{fontSize: '32px', paddingBottom: '0', justifyContent: 'center'}}>
+                    <Nav.Item>
+                        <Nav.Link href="https://www.linkedin.com/in/ahmed-akbarali-4799b570/" target="_blank">
+                            <img
+                                className="header-image"
+                                src={linkedin}
+                                alt="linkedin"
+                                style={{ filter: 'invert(1)'}}
+                            />
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="https://www.github.com/AhmedAkbarali" target="_blank">
+                            <img
+                                className="header-image"
+                                src={github}
+                                alt="github"
+                                style={{filter: 'invert(1)'}}
+                            />
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="mailto:ahmed.amin.akbarali@gmail.com" target="_blank">
+                            <img
+                                className="header-image"
+                                src={email}
+                                alt="email"
+                                style={{filter: 'invert(1)'}}
+                            />
+                        </Nav.Link>
+                    </Nav.Item>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default ContactMe;
---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import Home from '../Pages/Home';
+import SkillPage from '../Pages/SkillPage';
+import ContactMe from '../Pages/ContactMe';
+import WebProjectPage from "../Pages/WebProjectPage";
+import MobileProjectPage from "../Pages/MobileProjectPage";
+
+import './style/App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/webdev" exact>
+                    <WebProjectPage />
+                </Route>
+                <Route path="/mobdev" exact>
+                    <MobileProjectPage />
+                </Route>
+                <Route path="/skills" exact>
+                    <SkillPage />
+                </Route>
+                <Route path="/contact" exact>
+                    <ContactMe />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+  }
+}
+
+export default App;
