37f4c89e119c77cd6c742d70bbd20744e1361858
AhmedAkbarali <ahmed.amin.akbarali@gmail.com>
2020-02-24 04:22:10 -0500

    Implementing react-helmet and react-snapshot to improve SEO

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import MobileProjectPage from "../Pages/MobileProjectPage";
 import './style/App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Helmet} from "react-helmet/es/Helmet";
 
 
 
@@ -18,18 +19,33 @@ class App extends Component {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact>
+                    <Helmet>
+                        <title>Home</title>
+                    </Helmet>
                     <Home />
                 </Route>
                 <Route path="/webdev" exact>
+                    <Helmet>
+                        <title>Web Development</title>
+                    </Helmet>
                     <WebProjectPage />
                 </Route>
                 <Route path="/mobdev" exact>
+                    <Helmet>
+                        <title>Mobile Development</title>
+                    </Helmet>
                     <MobileProjectPage />
                 </Route>
                 <Route path="/skills" exact>
+                    <Helmet>
+                        <title>Skills</title>
+                    </Helmet>
                     <SkillPage />
                 </Route>
                 <Route path="/contact" exact>
+                    <Helmet>
+                        <title>Contact Me</title>
+                    </Helmet>
                     <ContactMe />
                 </Route>
             </Switch>
